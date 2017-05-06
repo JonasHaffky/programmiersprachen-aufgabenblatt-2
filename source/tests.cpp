96a038222054b127d8e9cb217554f94b150117c1
Jonas Haffky <jonas.david.benno.haffky@uni-weimar.de>
2017-05-06 20:41:06 +0200

    Add all tests for vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -73,6 +73,89 @@ TEST_CASE("describe_operator/=","[operator/=]")
   REQUIRE(c.x_==2.72f);
   REQUIRE(c.y_==0.2f);
 }
+TEST_CASE("describe_operator+","[operator+]")
+{ Vec2 a{{5.0f},{2.0f}};
+  Vec2 b{{4.0f},{0.0f}};
+  Vec2 c{{6.8f},{0.5f}};
+  Vec2 d{{0.7f},{4.8f}};
+  Vec2 temp_1{operator+(a,b)};
+  Vec2 temp_2{operator+(b,c)};
+  Vec2 temp_3{operator+(d,a)};
+  REQUIRE(temp_1.x_==9.0f);
+  REQUIRE(temp_1.y_==2.0f);
+  REQUIRE(temp_2.x_==10.8f);
+  REQUIRE(temp_2.y_==0.5f);
+  REQUIRE(temp_3.x_==5.7f);
+  REQUIRE(temp_3.y_==6.8f);
+}
+TEST_CASE("describe_operator-","[operator-]")
+{ Vec2 a{{5.0f},{2.0f}};
+  Vec2 b{{4.0f},{0.0f}};
+  Vec2 c{{0.2f},{0.5f}};
+  Vec2 d{{0.7f},{4.8f}};
+  Vec2 temp_1{operator-(a,b)};
+  Vec2 temp_2{operator-(b,a)};
+  Vec2 temp_3{operator-(d,c)};
+  REQUIRE(temp_1.x_==1.0f);
+  REQUIRE(temp_1.y_==2.0f);
+  REQUIRE(temp_2.x_==-1.0f);
+  REQUIRE(temp_2.y_==-2.0f);
+  REQUIRE(temp_3.x_==0.5f);
+  REQUIRE(temp_3.y_==4.3f);
+}
+TEST_CASE("describe_operator*","[operator*]")
+{ Vec2 a{{5.0f},{2.0f}};
+  Vec2 b{{4.0f},{0.0f}};
+  Vec2 c{{-3.0f},{1.0f}};
+  float x=1.3;
+  float y=0.4;
+  Vec2 temp_1{operator*(a,x)};
+  Vec2 temp_2{operator*(b,x)};
+  Vec2 temp_3{operator*(c,y)};
+  REQUIRE(temp_1.x_==6.5f);
+  REQUIRE(temp_1.y_==2.6f);
+  REQUIRE(temp_2.x_==5.2f);
+  REQUIRE(temp_2.y_==0.0f);
+  REQUIRE(temp_3.x_==-1.2f);
+  REQUIRE(temp_3.y_==0.4f);
+}
+TEST_CASE("describe_operator/","[operator/]")
+{ Vec2 a{{5.2f},{2.0f}};
+  Vec2 b{{4.0f},{0.0f}};
+  Vec2 c{{-3.0f},{1.0f}};
+  float x=4.0;
+  float y=0.4;
+  float z=0.0;
+  Vec2 temp_1{operator/(a,x)};
+  Vec2 temp_2{operator/(b,x)};
+  Vec2 temp_3{operator/(c,y)};
+  Vec2 temp_4{operator/(c,z)};
+  REQUIRE(temp_1.x_==1.3f);
+  REQUIRE(temp_1.y_==0.5f);
+  REQUIRE(temp_2.x_==1.0f);
+  REQUIRE(temp_2.y_==0.0f);
+  REQUIRE(temp_3.x_==-7.5f);
+  REQUIRE(temp_3.y_==2.5f);
+  REQUIRE(temp_4.x_==-3.0f);
+  REQUIRE(temp_4.y_==1.0f);
+}
+TEST_CASE("describe_operator*2","[operator*2]")
+{ Vec2 a{{5.0f},{2.0f}};
+  Vec2 b{{4.0f},{0.0f}};
+  Vec2 c{{-3.0f},{1.0f}};
+  float x=1.3;
+  float y=0.4;
+  Vec2 temp_1{operator*(x,a)};
+  Vec2 temp_2{operator*(x,b)};
+  Vec2 temp_3{operator*(y,c)};
+  REQUIRE(temp_1.x_==6.5f);
+  REQUIRE(temp_1.y_==2.6f);
+  REQUIRE(temp_2.x_==5.2f);
+  REQUIRE(temp_2.y_==0.0f);
+  REQUIRE(temp_3.x_==-1.2f);
+  REQUIRE(temp_3.y_==0.4f);
+}
+
 
 
 int main(int argc, char *argv[])
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -8,6 +8,11 @@ Vec2::Vec2(float a, float b):
     x_{a},
     y_{b}
 {}
+Vec2::Vec2(Vec2 const& v):
+    x_{v.x_},
+    y_{v.y_}
+{}
+
 Vec2& Vec2::operator+=(Vec2 const& v){
     x_+=v.x_;
     y_+=v.y_;
@@ -35,4 +40,38 @@ Vec2& Vec2::operator/=(float s){
     y_/=s;
     return *this;
    }
+}
+Vec2 operator+( Vec2 const & u , Vec2 const & v ){
+    float x=u.x_+v.x_;
+    float y=u.y_+v.y_;
+    Vec2 temp{{x},{y}};
+    return temp;
+}
+Vec2 operator-( Vec2 const & u , Vec2 const & v ){
+    float x=u.x_-v.x_;
+    float y=u.y_-v.y_;
+    Vec2 temp{x,y};
+    return temp;
+}
+Vec2 operator*( Vec2 const & v , float s ){
+    float x=v.x_*s;
+    float y=v.y_*s;
+    Vec2 temp{x,y};
+    return temp;
+}
+Vec2 operator/( Vec2 const & v , float s ){
+     if(s==0.0f){
+        std::cerr<<"Diese Operation ist nicht mit 0 moeglich!"<<std::endl;
+        return v;
+    }
+    else{
+    Vec2 temp{{v.x_/s},{v.y_/s}};
+    return temp;
+   }
+}
+Vec2 operator*( float s , Vec2 const & v ){
+    float x=v.x_*s;
+    float y=v.y_*s;
+    Vec2 temp{x,y};
+    return temp;
 }
